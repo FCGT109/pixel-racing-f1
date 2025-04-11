8d025393799cd9783668443ecd16358a7ea1401b
FCGT109 <205313889+FCGT109@users.noreply.github.com>
2025-04-11 13:28:17 +0200

    contador de voltes

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 var direccio := Vector2(0,-1)
 var velocitat := 200.0
 var acceleracio := 0
+var voltes:=0
+
+func voltes_portades():
+	voltes+=1
+
 func _ready() -> void:
 	position= Vector2(500,300)
 func _process(delta:float) -> void:
@@ -13,7 +18,7 @@ func _process(delta:float) -> void:
 
 	velocitat += mov * acceleracio * delta
 	velocity = direccio * velocitat 
-	$BlueLabel.text = "velocitat: %.2f\nnºvoltes: %.2f\ntemps: %.2f" % [velocitat, velocitat, velocitat]
+	$BlueLabel.text = "velocitat: %.2f\nnºvoltes: %.2f\ntemps: %.2f" % [velocitat, voltes, velocitat]
 	if Input.is_action_pressed("moure_esquerra"):
 		direccio = direccio.rotated(-0.05)
 	if Input.is_action_pressed("moure_dreta"):
@@ -24,7 +29,4 @@ func _process(delta:float) -> void:
 	
 func anima(direccio: Vector2,delta: float):
 	rotation = direccio.angle() + deg_to_rad(+90)
-	
-	
-	
-	
+		
